7fc87e14f8599da53e7749f9e8995bff58b4ad0d
zay-er <yarikmarietta@gmail.com>
2022-11-10 19:32:07 -0500

    Finished project

--- javascript.js
+++ javascript.js
@@ -1,26 +1,52 @@
-// I imagine I start with a felx-container and into that I insert
-const flexer = document.querySelector('.flex-container');
 
 
-function change(){
-    flexer.newDiv.style.backgroundColor = "black";
-}
+function createGrid(number){
+    const gridContainer = document.querySelector(".grid-container");
+    gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    for (let i = 1; i <= number * number; i++){
+        let div = document.createElement("div");
+        div.classList.add("grid-box")
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        gridContainer.appendChild(div);
 
-for(i = 0; i < 256; i++){
-    let newDiv =  document.createElement('div');
-    newDiv.className = 'box' + i;
+    }
+    document.querySelector(".grid.dimensions").textContent = `${number}x${number}`;
     
-    newDiv.addEventListener("mouseover", () =>{
-        newDiv.setAttribute('style', 'background-color: black;');
-    });
+}
 
-    flexer.appendChild(newDiv);
+// function changeColor(event){
+//     event.target.style.backgroundColor = "black";
+// }
 
+function resetGrid(){
+    let num = prompt("Please eneter grid size less than or equal to 100:");
+    if (num === undefined || num === null || num === "" ) {
+        return;
+    }
+
+    while (num > 100){
+        num = prompt("Number too big! Please enter grid size less than or equal to 100:");
+    }
+
+    const gridContainer = document.querySelector(".grid-container");
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    createGrid(num);
+    let gridBoxList = document.querySelectorAll(".grid-box");
+    gridBoxList.forEach(gridBox => {gridBox.addEventListener("mouseover", changeColor)});
+}
+
+function clearGrid(){
+    let gridBoxList = document.querySelectorAll(".grid-box");
+    gridBoxList.forEach(gridBox => {gridBox.style.backgroundColor = null});
 
 }
 
-
-const div = document.querySelector("div");
-square.addEventListener("mouseover", function(event) {
-  event.target.classList.replace("square", "color");
-});
+let gridNumber = 16;
+createGrid(gridNumber);
+// let gridBoxList = document.querySelectorAll(".grid-box");
+// gridBoxList.forEach(gridBox => {gridBox.addEventListener("mouseover", changeColor)});
